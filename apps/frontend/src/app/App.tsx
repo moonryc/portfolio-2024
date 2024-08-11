2817595d577cb99f6d815c19c9344b7bf5986420
Ryan <ryanmoondevelopment@gmail.com>
2024-08-11 18:45:36 -0500

    removed loading screen added background took care of many a issues with display

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,46 +1,43 @@
 import NavBar from './layouts/NavBar';
-import ParticleEngine from './layouts/ParticleEngine';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './layouts/HomePage';
 import ContactPage from './layouts/ContactPage';
 import ProjectsPage from './layouts/ProjectsPage';
 import TechnologiesPage from './layouts/TechnologiesPage';
 import AboutMePage from './layouts/AboutMePage';
 import { routes } from './routes/routes';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, useTheme } from '@mui/material';
 import { usePersonalQuery } from './hooks/usePersonalQuery';
-import LoadingScreen from './layouts/LoadingScreen';
+import BackgroundAnimation from './layouts/BackgroundAnimation';
 
 function App() {
+  const theme = useTheme();
+  const { value: personalData } = usePersonalQuery();
 
-  const {value:personalData} = usePersonalQuery()
-
-  if(!personalData){
-    return <LoadingScreen/>;
+  if (!personalData) {
+    return null;
   }
 
-  // if(personalData){
-  //   return <LoadingScreen/>;
-  // }
-
   return (
-    <Box px={"8px"}>
-      <ParticleEngine>
-        <Grid container height={"100vh"}>
-          <Grid item sm={12} md={4}>
+    <Box bgcolor={theme.palette.primary.main} position={'relative'} zIndex={1}>
+      <BackgroundAnimation />
+      <Box position={'relative'} zIndex={2}>
+        <Grid container height={'100vh'} overflow={'auto'} zIndex={1}>
+          <Grid item sm={12} md={4} paddingTop={[2, 4]} paddingLeft={[2, 4]} paddingRight={[2, 4, 0]}>
             <NavBar />
           </Grid>
-          <Grid display={"flex"} justifyContent={"center"} alignItems={"start"} mt={[2,0,0,8]} item sm={12} md={8}>
+          <Grid display={'flex'} justifyContent={'center'} alignItems={'start'} mt={[2, 2, 4, 8]} item sm={12} md={8}>
             <Routes>
               <Route path={routes.homePage.path} element={null} />
               <Route path={routes.about.path} element={<AboutMePage aboutMeDTO={personalData.aboutMe} />} />
               <Route path={routes.projects.path} element={<ProjectsPage projectsDTO={personalData.projects} />} />
-              <Route path={routes.technologies.path} element={<TechnologiesPage technologiesDTO={personalData.technologies} />} />
+              <Route path={routes.technologies.path}
+                     element={<TechnologiesPage technologiesDTO={personalData.technologies} />} />
               <Route path={routes.contact.path} element={<ContactPage contactDTO={personalData.contactMe} />} />
             </Routes>
           </Grid>
         </Grid>
-      </ParticleEngine>
+        {/*</ParticleEngine>*/}
+      </Box>
     </Box>
   );
 }
