b4ccc7682f21b97f36790fd4a97104148cf84c54
Ryan <ryanmoondevelopment@gmail.com>
2024-08-11 18:49:55 -0500

    stylized the page header

--- apps/frontend/src/app/layouts/PageHeader.tsx
+++ apps/frontend/src/app/layouts/PageHeader.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Typography, useTheme } from '@mui/material';
+import React, { useMemo } from 'react';
+import { Box, Typography, useTheme } from '@mui/material';
+import GridViewSharpIcon from '@mui/icons-material/GridViewSharp';
 
 type PageHeaderProps = {
   title: string;
@@ -7,8 +8,26 @@ type PageHeaderProps = {
 
 const PageHeader = ({title}: PageHeaderProps) => {
   const theme = useTheme()
+
+  const divider = useMemo(() => {
+    return <Box height={'4px'} width={'100%'} bgcolor={theme.palette.primary.main} />;
+  }, [theme.palette.primary.main]);
+
   return (
-    <Typography p={2} variant={"h3"}  textTransform={"uppercase"} color={theme.palette.primary.main} bgcolor={theme.palette.secondary.light}>{title}</Typography>
+
+  <Box
+    boxShadow={"5px 5px 10px black"}
+    sx={{
+      background: theme.palette.secondary.light,
+      py: '5px'
+    }}>
+    {divider}
+    <Box display={'flex'} alignItems={'center'} pl={'1rem'}>
+      <GridViewSharpIcon color={"primary"} sx={{ transform: `rotate(45deg)`}} />
+      <Typography p={1} variant={"h5"} fontWeight={"bold"} textTransform={"uppercase"} color={theme.palette.primary.main} bgcolor={theme.palette.secondary.light}>{title}</Typography>
+    </Box>
+    {divider}
+  </Box>
   );
 };
 
